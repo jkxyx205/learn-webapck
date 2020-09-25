0e1fdcc688617aa1c4a6630442e606849c007a54
rick <1050216579@qq.com>
2020-09-25 13:01:34 +0800

    use oneOf

--- webpack.config.js
+++ webpack.config.js
@@ -29,115 +29,121 @@ module.exports = {
       //   }
       // },
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      { 
-        test: /\.css$/, 
-        use: [
-          // { loader: 'style-loader' },
-          MiniCssExtractPlugin.loader,
-          // 'vue-style-loader', 
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
-          },
-          {
-            loader: 'postcss-loader'
-          },
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          // 'vue-style-loader',
-          {
-            loader: 'css-loader',
-            options: { 
-              // modules: true,
-              modules: {
-                localIdentName: "[name]__[local]___[hash:base64:5]"
-              }
-            }
-          },
-          'postcss-loader',
-          'sass-loader'
-        ]
-      },
-      // { test: /\.scss$/, 
-      //   use: [
-      //     { loader: 'style-loader' },
-      //     {
-      //       loader: 'css-loader',
-      //       options: {
-      //         modules: true
-      //       }
-      //     },
-      //     { loader: 'sass-loader' }
-      //   ]
-      // },
-      // { test: /\.(html|txt)$/, use: 'raw-loader' },
-      { test: /\.vue$/, use: 'vue-loader' },
-      // {
-      //   test: /\.(png|jpeg|jpg|gif)$/i,
-      //   use: 'file-loader'
-      // }
-      {
-        test: /\.svg$/,
-        include: path.resolve(__dirname, "src/assets/svg"),
-        use:[ 
-          { loader: 'svg-sprite-loader',
-            options: {
-              extract: false,
-              symbolId: filePath => path.basename(filePath),
-              outputPath: "static/svgIcons/",
-              publicPath: "static/svgIcons/",
-              spriteFilename: "svg-sprite.svg"
-            },
-          },
-          'svgo-loader'
-        ]
-      },
-      {
-        test: /\.(png|jpeg|jpg|gif)$/i,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 1024 * 12,
-            name: '[path][name][hash:8].[ext]', // file-loader配置项
-            esModule: false
-          }
-        }
-      },
-      {
-        test: /\.(woff2|woff|eot|ttf|svg)$/, // 字体文件
-        include: path.resolve(__dirname, "src/assets/font"),
-        loader: 'file-loader',
-        options: {
-          outputPath: 'font'
-        }
-      },
-      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'eslint-loader',
+        enforce: 'pre', // 优先执行
         options: {
           fix: false // 自动修复
         }
       },
+      { test: /\.vue$/, use: 'vue-loader' }, // .vue&.html 必须有一个放在根rule上，不能只放在oneOf => here is at least one root-level rule that matches .vue or .vue.html files.
       { // html中的img src能正确解析
         test: /\.html$/,
         loader: 'html-loader',
-      }
+      },
+      {
+        oneOf: [ // 匹配到loader，就不继续查找新到loader
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
+          { 
+            test: /\.css$/, 
+            use: [
+              // { loader: 'style-loader' },
+              MiniCssExtractPlugin.loader,
+              // 'vue-style-loader', 
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true
+                }
+              },
+              {
+                loader: 'postcss-loader'
+              },
+            ]
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              'style-loader',
+              // 'vue-style-loader',
+              {
+                loader: 'css-loader',
+                options: { 
+                  // modules: true,
+                  modules: {
+                    localIdentName: "[name]__[local]___[hash:base64:5]"
+                  }
+                }
+              },
+              'postcss-loader',
+              'sass-loader'
+            ]
+          },
+          // { test: /\.scss$/, 
+          //   use: [
+          //     { loader: 'style-loader' },
+          //     {
+          //       loader: 'css-loader',
+          //       options: {
+          //         modules: true
+          //       }
+          //     },
+          //     { loader: 'sass-loader' }
+          //   ]
+          // },
+          // { test: /\.(html|txt)$/, use: 'raw-loader' },
+          
+          // {
+          //   test: /\.(png|jpeg|jpg|gif)$/i,
+          //   use: 'file-loader'
+          // }
+          {
+            test: /\.svg$/,
+            include: path.resolve(__dirname, "src/assets/svg"),
+            use:[ 
+              { loader: 'svg-sprite-loader',
+                options: {
+                  extract: false,
+                  symbolId: filePath => path.basename(filePath),
+                  outputPath: "static/svgIcons/",
+                  publicPath: "static/svgIcons/",
+                  spriteFilename: "svg-sprite.svg"
+                },
+              },
+              'svgo-loader'
+            ]
+          },
+          {
+            test: /\.(png|jpeg|jpg|gif)$/i,
+            use: {
+              loader: 'url-loader',
+              options: {
+                limit: 1024 * 12,
+                name: '[path][name][hash:8].[ext]', // file-loader配置项
+                esModule: false
+              }
+            }
+          },
+          {
+            test: /\.(woff2|woff|eot|ttf|svg)$/, // 字体文件
+            include: path.resolve(__dirname, "src/assets/font"),
+            loader: 'file-loader',
+            options: {
+              outputPath: 'font'
+            }
+          }
+        ]
+      },
     ]
   },
   plugins: [
