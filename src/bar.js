5a69da08f19d73e84ed771e5b191f9ee30bb4ae3
rick <1050216579@qq.com>
2020-09-25 14:21:25 +0800

    import/export/export default

--- src/bar.js
+++ src/bar.js
@@ -1,5 +1,15 @@
+import math, { mul } from './math'; // export default 导出的不能用{ add } 来接收; export 必须需要用{}接收
+
+import * as utils from './utils'; // export 必须需要用{}接收或者"* as urils"; 不能直接import utils from ...
+// import { toLowerCase } from './utils';
+
 const sayHello = () => {
   console.log('hello this is from bar....');
+  console.log(math.add(50, 50));
+  console.log(mul(9, 9));
+  // eslint-disable-next-line
+  console.log(utils.toLowerCase('HELLO CHINA'));
+  // console.log(toLowerCase('HELLO WORLD'));
 };
 
 export default function bar() {
---
+++ src/math.js
@@ -0,0 +1,12 @@
+export default {
+  add(a, b) {
+    return a + b;
+  },
+  minus(a, b) {
+    return a - b;
+  }
+};
+
+export function mul(a, b) {
+  return a * b;
+}
